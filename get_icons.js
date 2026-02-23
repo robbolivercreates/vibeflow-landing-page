f0345f10c68a77c7b7267542ee0c4c7d0ab75d42
Rob Boliver <contato@robboliver.pro>
2026-02-23 12:33:35 +0000

    chore: remove temp get_icons.js script

--- get_icons.js
+++
@@ -1,8 +0,0 @@
-const https = require('https');
-['openai', 'canva', 'trello', 'microsoftteams'].forEach(name => {
-    https.get('https://unpkg.com/simple-icons@v14/icons/' + name + '.svg', res => {
-        let data = '';
-        res.on('data', chunk => data += chunk);
-        res.on('end', () => console.log(name + ' SVG:\n' + data + '\n'));
-    });
-});
